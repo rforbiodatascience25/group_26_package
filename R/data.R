71ba0a100f24acd10cf54ed3a7d77cccb5f1644f
lunsusa <s253705@dtu.dk>
2025-10-28 10:03:01 +0100

    Updates.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Standard Codon Table
+#'
+#' Standard codone table of aminoacids.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codonTable"
